7dfcfeef8576f57ef0c07de437a7d2bcd88b6f15
Vanderson Vieira <vanderson.vieira85@gmail.com>
2025-03-19 14:44:45 -0300

    update

--- src/validador.py
+++ src/validador.py
@@ -1,36 +1,39 @@
 from pydantic import BaseModel, Field
 
 class Estatisticas(BaseModel):
-    id: float = Field(validate_default=True)  # Real number (ℝ)
-    cliente: str = Field(validate_default=True)  # Text
-    email: str = Field(validate_default=True)  # Text
-    ddd: float = Field(validate_default=True)  # Real number (ℝ)
-    telefone: float = Field(validate_default=True)  # Real number (ℝ)
-    id_imovel: float = Field(validate_default=True)  # Real number (ℝ)
-    imovel: str = Field(validate_default=True)  # Text
-    plano: str = Field(validate_default=True)  # Categorical
-    cidade: str = Field(validate_default=True)  # Categorical
-    estado: str = Field(validate_default=True)  # Categorical
-    id_anunciante: float = Field(validate_default=True)  # Real number (ℝ)
-    anunciante: str = Field(validate_default=True)  # Text
-    id_contratante: str = Field(validate_default=True)  # Categorical
-    contratante: str = Field(validate_default=True)  # Categorical
-    id_campanha: float = Field(validate_default=True)  # Real number (ℝ)
-    campanha: str = Field(validate_default=True)  # Text
-    tipo: str = Field(validate_default=True)  # Categorical
-    mensagem: str = Field(validate_default=True)  # Text
-    canal: str = Field(validate_default=True)  # Categorical
-    recibo: str = Field(validate_default=True)  # Text
-    bonificado: str = Field(validate_default=True)  # Categorical
-    motivo_bonificacao: str = Field(validate_default=True)  # Categorical
-    elogio: str = Field(validate_default=True)  # Categorical
-    critica: str = Field(validate_default=True)  # Categorical
-    repescagem: str = Field(validate_default=True)  # Categorical
-    criado: float = Field(validate_default=True)  # Real number (ℝ)
-    utm_source: str = Field(validate_default=True)  # Categorical
-    utm_medium: str = Field(validate_default=True)  # Categorical
-    utm_campaign: str = Field(validate_default=True)  # Categorical
-    utm_content: str = Field(validate_default=True)  # Categorical
-    ip: str = Field(validate_default=True)  # Text
-    ultima_atualizacao: float = Field(validate_default=True)  # Real number (ℝ)
-    data: float = Field(validate_default=True)  # Real number (ℝ)
+    id: int = Field(...)  # Real number (ℝ)
+    cliente: str = Field(...)  # Text
+    email: str = Field(...)  # Text
+    ddd: int = Field(...)  # Real number (ℝ)
+    telefone: str = Field(...)  # Real number (ℝ)
+    id_imovel: int = Field(...)  # Real number (ℝ)
+    imovel: str = Field(...)  # Text
+    plano: str = Field(...)  # Categorical
+    cidade: str = Field(...)  # Categorical
+    estado: str = Field(...)  # Categorical
+    id_anunciante: int = Field(...)  # Real number (ℝ)
+    anunciante: str = Field(...)  # Text
+    id_contratante: int = Field(...)  # Categorical
+    contratante: str = Field(...)  # Categorical
+    id_campanha: int = Field(..., nullable=True)  # Real number (ℝ)
+    campanha: str = Field(..., nullable=True)  # Text
+    tipo: str = Field(...)  # Categorical
+    mensagem: str = Field(...)  # Text
+    canal: str = Field(...)  # Categorical
+    recibo: str = Field(..., nullable=True)  # Text
+    bonificado: str = Field(...)  # Categorical
+    motivo_bonificacao: str = Field(..., nullable=True)  # Categorical
+    elogio: str = Field(...)  # Categorical
+    critica: str = Field(...)  # Categorical
+    repescagem: str = Field(...)  # Categorical
+    criado: int = Field(...)  # Real number (ℝ)
+    utm_source: str = Field(...)  # Categorical
+    utm_medium: str = Field(...)  # Categorical
+    utm_campaign: str = Field(..., nullable=True)  # Categorical
+    utm_content: str = Field(..., nullable=True)  # Categorical
+    ip: str = Field(...)  # Text
+    ultima_atualizacao: int = Field(...)  # Real number (ℝ)
+    data: int = Field(...)  # Real number (ℝ)
+
+class Config:
+        validate_default = True
